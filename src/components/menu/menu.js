20367bd099d6561081fd206d7ef97288edfaf322
Mikaël Léger <mikael.leger8@gmail.com>
2024-05-01 23:38:58 +0200

    ui/better menu

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,18 +1,19 @@
 import Link from "next/link";
 
 import "./menu.scss";
+import Button from "../button/button";
 
 export default function Menu() {
   return (
     <div className="menu">
       <div className="menu-item">
-        <Link href="/products">Products</Link>
+        <Button bgColor="primary" size="big" value={<Link href="/products">Products</Link>} />
       </div>
       <div className="menu-item">
-        <Link href="/data">Data</Link>
+        <Button bgColor="primary" size="big" value={<Link href="/data">Data</Link>} />
       </div>
       <div className="menu-item">
-        <Link href="/">DLC Asso</Link>
+        <Button bgColor="primary" size="big" value={<Link href="/">DLC Asso</Link>} />
       </div>
     </div>
   );
